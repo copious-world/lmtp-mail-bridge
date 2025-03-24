23067460c29926fbe701484b0ae9b80e5fb33433
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2025-03-23 17:46:38 -0700

    quick test - not a thing yet

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# lmtp-mail-bridge
+
+
+Allows a local stmp server to pass data through an lmtp server to the copious endpoint servers.
+
---
+++ index.js
@@ -0,0 +1,16 @@
+
+// smtp.js
+const {SMTPServer} = require('smtp-server');
+
+const server = new SMTPServer({
+    // disable STARTTLS to allow authentication in clear text mode
+    disabledCommands: ['STARTTLS', 'AUTH'],
+    logger: true,
+    onData(stream, session, callback){
+        stream.pipe(process.stdout); // print message to console
+        stream.on('end', callback);
+    },
+});
+
+server.listen(25);
+
